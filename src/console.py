a17313566c4b95547e4a46b1270c176d82dbfbea
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-07-11 17:53:16 +0300

    Wrote the update_product_price() function to interact with the user in the console.py module.

---
+++ src/console.py
@@ -0,0 +1,38 @@
+from exceptions import InvalidPriceError, PriceDecreaseError
+from models.product import Product
+
+
+def update_product_price(product: Product) -> bool:
+    """
+    Функция для изменения цены продукта.
+    Получает на вход объект класса Product и вероятно будущую цену.
+    Если предложенная цена соответствует критериям, то значение цены переназначается.
+    Если цена меньше или равна нулю, то возникает ошибка.
+    Если цена больше нуля, но меньше текущей, то пользователю задается вопрос, желает ли он понизить цену.
+    В зависимости от пользовательского вопроса, цена, либо изменяется, либо остается прежней.
+
+    :param product: Объект класса Product
+    :return: True/False
+    """
+
+    try:
+        product.price = float(input("Введите новую цену: "))
+        print("Цена успешно обновлена")
+        return True
+
+    except InvalidPriceError as err:
+        print(f"Ошибка: {err}")
+        return False
+
+    except PriceDecreaseError as err:
+        print(err)
+        answer = input("Хотите понизить цену??? Введите 'y' если (да) или 'n' если (нет)").lower().strip()
+
+        if answer == "y":
+            product.force_price_update(err.new)
+            print("Цена понижена.")
+            return True
+
+        else:
+            print("Цена осталась прежней.")
+            return False
