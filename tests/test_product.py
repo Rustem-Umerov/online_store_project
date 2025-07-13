0587214fa0bb8574692119d458cb458f945f4bcc
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-07-13 14:28:23 +0300

    i wrote the test for product class in the product.py module

--- tests/test_product.py
+++ tests/test_product.py
@@ -2,6 +2,7 @@ import pytest
 from _pytest.monkeypatch import MonkeyPatch
 
 from models.product import Product, validate_non_negative
+from src.exceptions import InvalidPriceError, PriceDecreaseError
 
 
 def test_product(product: Product) -> None:
@@ -60,6 +61,86 @@ def test_init_calls_validate_non_negative(monkeypatch: MonkeyPatch) -> None:
 
     assert p.price == 99.99
     assert p.quantity == 5
+
+
+def test_creation_new_product() -> None:
+    """Тест проверяет, что класс-метод new_product успешно добавляет новый товар в список."""
+
+    new_prod = Product.new_product(name="a", description="aaa", price=10.00, quantity=1, existing_products=[])
+
+    assert isinstance(new_prod, Product)
+    assert new_prod.name == "a"
+    assert new_prod.description == "aaa"
+    assert new_prod.price == 10.00
+    assert new_prod.quantity == 1
+
+
+def test_update_new_product() -> None:
+    """Тест проверяет, что класс-метод new_product успешно обновляет товар в списке."""
+
+    list_product = [
+        Product(name="a", description="aaa", price=10.00, quantity=1),
+        Product(name="b", description="bbb", price=20.00, quantity=2),
+    ]
+
+    prod = Product.new_product(name="b", description="bbb", price=30.00, quantity=10, existing_products=list_product)
+
+    assert isinstance(prod, Product)
+    assert len(list_product) == 2
+    assert list_product[1].name == "b"
+    assert list_product[1].description == "bbb"
+    assert list_product[1].price == 30.00
+    assert list_product[1].quantity == 12
+    assert prod is list_product[1]
+
+
+@pytest.mark.parametrize(
+    "new_price, result",
+    [
+        (200.00, None),
+        (0, [InvalidPriceError, "Недопустимая цена: 0. Цена не должна быть нулевой или отрицательной."]),
+        (-5, [InvalidPriceError, "Недопустимая цена: -5. Цена не должна быть нулевой или отрицательной."]),
+        (50.0, [PriceDecreaseError, "Вы ввели цену: 50.0, меньше текущей: 99.99."]),
+    ],
+)
+def test_new_price(product: Product, new_price: float, result: list) -> None:
+    """
+    Проверяет setter price:
+    - успешное обновление, если expected is None
+    - выброс корректного исключения
+    """
+
+    if result is None:
+        product.price = new_price
+        assert product.price == new_price
+
+    else:
+        exc_type, exc_message = result
+        with pytest.raises(exc_type) as exc:
+            product.price = new_price
+
+        assert exc_message in str(exc.value)
+
+        if exc_type is PriceDecreaseError:
+            assert exc.value.old == 99.99
+            assert exc.value.new == 50.0
+
+
+@pytest.mark.parametrize(
+    "new_price",
+    [
+        10.0,
+        20.0,
+        30.0,
+        40.0,
+    ],
+)
+def test_force_price_update(product: Product, new_price: float) -> None:
+    """Тест проверяет принудительное обновление цены без срабатывания исключения через метод force_price_update."""
+
+    product.force_price_update(new_price=new_price)
+
+    assert product.price == new_price
 
 
 @pytest.mark.parametrize(
