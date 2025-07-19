780bf09f3f9d31cab6680d31779323b325bc9f7c
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-07-19 14:40:27 +0300

    wrote a ProductIterator class to iterate over the list of products from the Category class in the product_iterator.py module

---
+++ models/product_iterator.py
@@ -0,0 +1,40 @@
+from models.category import Category
+from models.product import Product
+from typing import Iterator
+
+
+class ProductIterator:
+    """
+    Класс ProductIterator перебирает товары одной категории.
+    Класс принимает на вход объект класса Category и производит итерацию по товарам(объектам класса Product),
+    которые хранятся в данной категории.
+    """
+
+    category: Category
+
+    def __init__(self, category_obj: Category) -> None:
+        """
+        Инициализирует итератор.
+
+        :param category_obj: Объект класса Category.
+        """
+
+        self._category = category_obj
+        self._index = 0
+
+    def __iter__(self) -> Iterator[Product]:
+        """Магический метод, который возвращает объект итератора."""
+
+        self._index = 0
+        return self
+
+    def __next__(self) -> Product:
+        """Магический метод, возвращает следующий элемент последовательности."""
+
+        if self._index >= len(self.category.products):
+            raise StopIteration
+
+        product = self._category.products[self._index]
+        self._index += 1
+        return product
+
