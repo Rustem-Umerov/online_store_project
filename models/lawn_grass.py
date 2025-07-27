00469ed0f9298e7397f6e348dae9d2c0107a2357
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-07-27 11:59:44 +0300

    fixed minor errors in the code

--- models/lawn_grass.py
+++ models/lawn_grass.py
@@ -32,7 +32,7 @@ class LawnGrass(Product):
         super().__init__(name, description, price, quantity)
         self.country = country
         self.germination_period = germination_period
-        self.country = color
+        self.color = color
 
     def __add__(self, other: object) -> float:
         """Вывод полной стоимости всех товаров на складе"""
