4cb02379854651eb12e766b5f2f75d7c2c665e91
Rustem Umerov <rustem.umerov.00@yandex.ru>
2025-07-30 12:13:45 +0300

    in the PrintMixin class (print_mixin.py module) fixed errors pointed out by mypy

--- models/print_mixin.py
+++ models/print_mixin.py
@@ -1,3 +1,6 @@
+from typing import Any
+
+
 class PrintMixin:
     """Класс-миксин - при создании объекта, то есть при работе метода __init__,
     печатает в консоль информацию о том, от какого класса и с какими параметрами был создан объект.
@@ -8,15 +11,14 @@ class PrintMixin:
     _price: float
     quantity: int
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
         """Инициализация вывода информации, в консоль, полученной от __repr__."""
 
         super().__init__(*args, **kwargs)
         print(repr(self))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """Магический метод, для отображения информации об объекте класса в режиме отладки (для разработчиков)
         Выводит информацию в виде: Product('Продукт1', 'Описание продукта', 1200, 10)"""
 
         return f"{self.__class__.__name__}('{self.name}', '{self.description}', {self._price}, {self.quantity})"
-
